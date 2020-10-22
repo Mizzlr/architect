91447b8c0dc73494c36cec3b7ad614a03a0fe213
Mizzlr <mushtaquethegeek@gmail.com>
2020-10-22 21:05:48 +0530

    Add some implementation for analyzing dependencies.

--- architect/cli.py
+++ architect/cli.py
@@ -1,18 +1,88 @@
 import json
+import re
+from collections import defaultdict
+from fnmatch import fnmatch
 
-from architect.complexity import run_lizard
-from architect.utils import scan_imports, ctags, cloc_stats, get_input_folder
+from pygments.lexers import get_all_lexers
+
+from architect.utils import scan_imports, ctags, cloc_stats, get_input_folder, run_lizard
+
+
+def ctags_by_lang_ext(exports, lang_ext='.py'):
+    return {k: v for k, v in exports.items() if k.endswith(lang_ext)}
+
+
+def lang_exts_for(langs):
+    lang_exts = []
+    ls = [lang.strip() for lang in langs.split(',') if lang.strip()]
+    for lang, langs, exts, mimes in get_all_lexers():
+        for l in ls:
+            if l.lower() in langs or f'*.{l}' in exts:
+                lang_exts.extend(exts)
+    return lang_exts
+
+
+def file_match_exts(file, lang_exts):
+    return any([fnmatch(file, ext) for ext in lang_exts])
+
+
+def analyze_dependency(import_stmt_regex, import_stmt, source_file, possible_target_files, exports):
+    """
+    Compute the subset of possible_target_files that the source file actually depends on.
+    Heuritics to compute the dependency is based on the the language, and semantics of the import statement
+    """
+
+    return set()
+
+
+def analyze_dependencies(imports, exports):
+    """
+    imports: Data about what a file declares as its dependency, using an import, include, require etc.
+        The exact import syntax and semantic depends on the language.
+
+    exports: Data abaut what a file exports, the things declared in a file
+        exports is map (dict) of file to list of ctags from that file
+        each ctag dict has token, regex, kind, scope (scope is optional)
+        `token` is the textual content of the identifier
+        `regex` is a js-style regex that when searched, we can find the line number of code containing this tag
+        `kind` is v, f, m, c stands for variable, file, method, class
+        `scope` is the parent containing (wrapping) object, class for method,
+        a method or function for variable if available
+
+    return:
+        Compute a graph of dependency file1 -> file2, if file2 exports something that file1 imports
+        Graph is a adjancency list (dict, mapping file to list of files it depends on)
+    """
+
+    dependency_graph = defaultdict(set)
+
+    for (import_stmt_regex, langs), file_imports in imports:
+        import_stmt_regex = re.compile(import_stmt_regex)
+        lang_exts = lang_exts_for(langs)
+        possible_target_files = [file for file in exports if file_match_exts(file, lang_exts)]
+
+        for file_import in file_imports:
+            source_file = file_import['file']
+            import_stmt = file_import['import_stmt']
+            target_files = analyze_dependency(import_stmt_regex, import_stmt, source_file, possible_target_files, exports)
+            dependency_graph[source_file].update(target_files)
+
+    return {k: list(v) for k, v in dependency_graph.items()}
+
 
 if __name__ == '__main__':
+    print(lang_exts_for('java,py,js,rust,rb,php'))
     folder = get_input_folder('~/cn-sococo-blueprints')
-    size = cloc_stats(folder)
+    # size = cloc_stats(folder)
     imports = scan_imports(folder)
     exports = ctags(folder)
-    metrics = run_lizard(folder)
+    dependencies = analyze_dependencies(imports, exports)
+    # metrics = run_lizard(folder)
 
     json.dump({
-        'size': size,
+        # 'size': size,
         'imports': imports,
         'exports': exports,
-        'metrics': metrics,
+        'dependencies': dependencies,
+        # 'metrics': metrics,
     }, open('output.json', 'w'), indent=4)
--- architect/utils.py
+++ architect/utils.py
@@ -7,6 +7,8 @@ import shutil
 import subprocess
 import sys
 import tempfile
+from collections import defaultdict
+from concurrent import futures as cf
 
 import lizard
 from lizard_ext import lizardduplicate
@@ -46,7 +48,7 @@ def ctags(folder, languages='ada,c,c#,c++,clojure,cobol,elixir,erlang,fortran,go
     cwd = os.getcwd()
     os.chdir(dir)
     exec_command(f'ctags -R --output-format=u-ctags --languages="{languages}" --totals {folder}')
-    tags = []
+    tags = defaultdict(list)
     preamble = True
     with open('tags', 'rb') as tags_file:
         for line in tags_file:
@@ -55,13 +57,18 @@ def ctags(folder, languages='ada,c,c#,c++,clojure,cobol,elixir,erlang,fortran,go
                 preamble = False
             if not preamble:
                 parts = line.split('\t')
-                scope = parts[5] if len(parts) > 5 else None
-                kind = parts[4] if len(parts) > 4 else parts[-1]
-                tag = dict(token=parts[0], file=parts[1], regex=parts[3], kind=kind, scope=scope)
-                tags.append(tag)
+                if len(parts[-1]) == 1:
+                    kind = parts[-1]
+                    scope = None
+                else:
+                    kind = parts[-2]
+                    scope = parts[-1]
+                file = parts[1]
+                tag = dict(token=parts[0], regex=parts[2], kind=kind, scope=scope)
+                tags[file].append(tag)
     os.chdir(cwd)
     shutil.rmtree(dir)
-    return tags
+    return {k: v for k, v in tags.items()}
 
 
 # https://rosettacode.org/wiki/Include_a_file see this for more languages
@@ -73,29 +80,34 @@ IMPORT_HINTS = [
     (r'\bopen\b(.*)',  'ocaml'),
     (r'\busing\b(.*)',  'csharp'),
     (r'\bsource\b(.*)', 'sh'),
-    (r'\bload\b(.*)', 'clojure,lua'),
+    (r'\bload\b(.*)', 'clojure,lua,ruby'),
     (r'\blibrary\b(.*)', 'r'),
     (r'\binclude_once\((.*)\)', 'php'),
     (r'\brequire_once\((.*)\)',  'php'),
     (r'\brequire\((.*)\)', 'js,php,ruby'),
-    (r'\brequire\b(.*)', 'js,php'),
-    (r'#\s*include\b(.*)',  'c,cpp'),
+    (r'\brequire\b(.*)', 'js,ruby'),
+    (r'#?\s*include\b(.*)',  'c,cpp,ruby'),
     (r'\bextern crate\b(.*)', 'rust'),
+    (r'\bextend\b(.*)', 'ruby'),
 ]
 
 
 def scan_imports(folder):
-    imports = {}
+    all_imports = {}
 
     for regex, lang in tqdm(IMPORT_HINTS, desc='Scanning imports'):
+        lang_types = ' '.join([f'-t {lang.strip()}' for lang in lang.split(',') if lang.strip()])
+        imports = []
         try:
-            output = exec_command(f'rg -t {lang} "{regex}" {folder}').splitlines(keepends=False)
+            output = exec_command(f'rg --no-ignore --glob "!**/*.min.js" {lang_types} "{regex}" {folder}')
+            for line in output.splitlines(keepends=False):
+                file, import_stmt = line.split(':', maxsplit=1)
+                imports.append(dict(file=file, import_stmt=import_stmt))
         except subprocess.CalledProcessError:
             logging.debug(f'Silently ignoring no match for {lang}, {regex}')
-            output = []
-        imports[(regex, lang)] = output
 
-    return list(imports.items())
+        all_imports[(regex, lang)] = imports
+    return list(all_imports.items())
 
 
 class DuplcodeExtension(lizardduplicate.LizardExtension):
@@ -114,3 +126,84 @@ def get_input_folder(default):
         folder = os.path.expanduser(folder)
     folder = os.path.abspath(os.path.normpath(folder))
     return folder
+
+
+def func_info_to_dict(func_info: lizard.FunctionInfo):
+    return {
+        'func_name': func_info.name,
+        'func_signature': func_info.long_name,
+        'start_line': func_info.start_line,
+        'end_line': func_info.end_line,
+        'lines': func_info.nloc,
+        'complexity': func_info.cyclomatic_complexity,
+        'max_nesting_depth': func_info.max_nesting_depth,
+    }
+
+
+def file_info_to_dict(file_info: lizard.FileInformation):
+    return {
+        'file': file_info.filename,
+        'lines': file_info.nloc,
+        'tokens': file_info.token_count,
+        'avg_lines': file_info.average_nloc,
+        'avg_tokens': file_info.average_token_count,
+        'avg_complexity': file_info.average_cyclomatic_complexity,
+        'sum_complexity': file_info.CCN,
+        'sum_max_nesting_depth': file_info.ND,
+        'word_count': file_info.wordCount,
+        'functions': [func_info_to_dict(func) for func in file_info.function_list],
+    }
+
+
+def snippet_to_dict(snippet):
+    return {'file': snippet.file_name, 'start_line': snippet.start_line, 'end_line': snippet.end_line}
+
+
+def run_lizard(folder, concurrency=4, find_duplicates=True):
+    duplcode_ext = architect.utils.DuplcodeExtension()
+    from importlib import import_module as im
+    wordcount_ext = im('lizard_ext.lizardwordcount').LizardExtension()
+
+    extensions = lizard.get_extensions(['mccabe', 'nd']) + [wordcount_ext]
+    if find_duplicates:
+        extensions.append(duplcode_ext)
+
+    # extensions = [lizard.preprocessing, lizard.line_counter, duplcode_ext]
+    files = find_files(folder, for_lizard=True, lang_ext='')
+    file_analyzer = lizard.FileAnalyzer(extensions)
+
+    with cf.ProcessPoolExecutor(max_workers=concurrency) as executor:
+        futures = {}
+        complexity_by_file = {}
+
+        for file in files:
+            futures[executor.submit(file_analyzer, file)] = file
+
+        for future in tqdm(cf.as_completed(futures), total=len(futures), desc='Analyzing complexity in files'):
+            file = futures[future]
+            logging.debug(f'Analyzed complexity for file: {file}')
+            if future._exception:
+                logging.warning(f'Failed to analyze complexity for file: {file}')
+            else:
+                file_info: lizard.FileInformation = future.result()
+                complexity_by_file[file] = file_info
+
+        lizard_metrics = {'complexity_by_file': {k: file_info_to_dict(v) for k, v in complexity_by_file.items()}}
+        if find_duplicates:
+            list(duplcode_ext.cross_file_process(complexity_by_file.values()))
+
+            duplicate_blocks = []
+            for duplicate_block in tqdm(duplcode_ext.get_duplicates(), desc='Analyzing duplicate code:'):
+                duplicate_blocks.append([snippet_to_dict(snippet) for snippet in duplicate_block])
+
+            logging.info(f'Total duplicate blocks: {len(duplicate_blocks)}')
+            logging.info("Total duplicate rate: %.2f%%" % (duplcode_ext.duplicate_rate() * 100))
+            logging.info("Total unique rate: %.2f%%" % (duplcode_ext.unique_rate() * 100))
+
+            lizard_metrics.update({
+                'duplicate_blocks': duplicate_blocks,
+                'duplicate_blocks_count': len(duplicate_blocks),
+                'duplicate_rate': duplcode_ext.duplicate_rate(),
+                'unique_rate': duplcode_ext.unique_rate()
+            })
+        return lizard_metrics
